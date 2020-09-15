6def2bace7af1cf113293372cee7032cfeabd4f3
QuocTuanJV <quoctuan.naf@gmail.com>
2020-09-15 08:55:22 +0700

    up git hub

---
+++ stock-ui/src/main/java/com/tuanlq/demo/stockui/ChartController.java
@@ -0,0 +1,12 @@
+package com.tuanlq.demo.stockui;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChartController {
+    @FXML
+    public LineChart<String, Double> chart;
+
+}
